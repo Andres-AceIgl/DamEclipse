0356b397aae6d4627a2300f9ff4b1df6bc602dd5
Andres <andres.aceigl@educa.jcyl.es>
2021-04-16 12:23:28 +0200

    Nueva subida en cambio1

--- Prueba1/src/paquete/Principal.java
+++ Prueba1/src/paquete/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo Proyecto Git-Java");
 		System.out.println("Hola Mundo de nuevo (2)");
+		System.out.println("Cambio en cambio 1");
 
 	}
 
